4faa26c47c944caa5977738795aec1dc2ac36517
alkanaziz <alkanaziz@gmail.com>
2024-10-08 13:51:52 +0200

    Refactor AdminPage component to include UserCard component and layout for user cards

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,22 @@
+import UserCard from "@/components/UserCard";
+
 const AdminPage = () => {
   return (
-    <div className=''>AdminPage</div>
-  )
-}
+    <div className="flex flex-col gap-4 p-4 lg:flex-row">
+      {/* LEFT */}
+      <div className="lg:w-2/3">
+        {/* USER CARD */}
+        <div className="flex flex-col gap-4 justify-between lg:flex-row">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="bg-blue-200 lg:w-1/3">r</div>
+    </div>
+  );
+};
 
-export default AdminPage
+export default AdminPage;
