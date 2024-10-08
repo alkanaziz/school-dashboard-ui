4aa217d14b5b939a3332383735c0496fa0245aac
alkanaziz <alkanaziz@gmail.com>
2024-10-08 17:33:11 +0200

    Refactor UserCard component layout and styling

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,16 +1,18 @@
-import Image from "next/image"
+import Image from "next/image";
 
-const UserCard = ({type}:{type:string}) => {
+const UserCard = ({ type }: { type: string }) => {
   return (
-    <div className='rounded-2xl odd:bg-privatPurple flex flex-col gap-6 even:bg-privatSky flex-1 p-4 min-w-[130px]'>
-      <div className="flex justify-between items-center">
-        <span className="text-sm text-slate-600 bg-slate-100 px-2 py-1 rounded-full">2024/25</span>
+    <div className="flex min-w-[130px] flex-1 flex-col gap-6 rounded-2xl p-4 odd:bg-privatPurple even:bg-privatYellow">
+      <div className="flex items-center justify-between">
+        <span className="rounded-full bg-slate-100 px-2 py-1 text-sm text-slate-600">
+          2024/25
+        </span>
         <Image src="/more.png" alt="more icon" width={20} height={20} />
       </div>
       <h1 className="text-2xl">3545</h1>
-      <h2 className="capitalize text-sm font-medium text-slate-500">{type}s</h2>
+      <h2 className="text-sm font-medium capitalize text-slate-500">{type}s</h2>
     </div>
-  )
-}
+  );
+};
 
-export default UserCard
+export default UserCard;
