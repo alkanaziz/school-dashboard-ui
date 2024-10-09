3865c43d74b25c8b613c45ee0161ae999dc9bd3a
alkanaziz <alkanaziz@gmail.com>
2024-10-09 02:22:56 +0200

    Add charts components for displaying student count, attendance, and financial data

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,16 +1,34 @@
+import AttendanceChart from "@/components/AttendanceChart";
+import CountCharts from "@/components/CountCharts";
+import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 
 const AdminPage = () => {
   return (
     <div className="flex flex-col gap-4 p-4 lg:flex-row">
       {/* LEFT */}
-      <div className="lg:w-2/3">
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
         {/* USER CARD */}
-        <div className="flex justify-between gap-4 flex-wrap">
+        <div className="flex flex-wrap justify-between gap-4">
           <UserCard type="student" />
           <UserCard type="teacher" />
           <UserCard type="parent" />
           <UserCard type="staff" />
+        </div>
+        {/* MIDDLE CHART */}
+        <div className="middle-chart flex flex-col gap-4 lg:flex-row">
+          {/* COUNT CHART */}
+          <div className="h-[450px] w-full lg:w-1/3">
+            <CountCharts />
+          </div>
+          {/* ATTENDANCE CHART */}
+          <div className="h-[450px] w-full lg:w-2/3">
+            <AttendanceChart />
+          </div>
+        </div>
+        {/* BOTTOM CHART */}
+        <div className="bottom-chart w-full h-[500px]">
+          <FinanceChart />
         </div>
       </div>
       {/* RIGHT */}
