b107b1d912adc5837ce1fa48e890c1f425df1664
alkanaziz <alkanaziz@gmail.com>
2024-10-17 15:10:52 +0200

    Refactor TeacherPage component to add BigCalendar and Announcements components

--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,7 +1,22 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+
 const TeacherPage = () => {
   return (
-    <div className=''>TeacherPage</div>
-  )
-}
+    <div className="flex flex-1 flex-col gap-4 p-4 xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full rounded-md bg-white p-4">
+          <h1 className="text-xl font-semibold">Schedule</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="flex w-full flex-col gap-8 xl:w-1/3">
+        <Announcements />
+      </div>
+    </div>
+  );
+};
 
-export default TeacherPage
+export default TeacherPage;
