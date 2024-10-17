56207774cffd16e0492d3d57a9dc1c19d10e0c88
alkanaziz <alkanaziz@gmail.com>
2024-10-17 15:10:38 +0200

    Refactor DashboardLayout component styles

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -11,7 +11,7 @@ export default function DashboardLayout({
   return (
     <div className="flex h-screen">
       {/* LEFT */}
-      <div className="w-[14%] overflow-scroll no-scrollbar md:w-[8%] lg:w-1/6 xl:w-[14%]">
+      <div className="no-scrollbar w-[14%] overflow-scroll md:w-[8%] lg:w-1/6 xl:w-[14%]">
         <Link
           href="/"
           className="flex items-center justify-center gap-2 p-4 lg:justify-start"
@@ -22,7 +22,7 @@ export default function DashboardLayout({
         <Menu />
       </div>
       {/* RIGHT */}
-      <div className="w-[86%] overflow-scroll no-scrollbar bg-slate-100 md:w-[92%] lg:w-5/6 xl:w-[86%]">
+      <div className="no-scrollbar flex w-[86%] flex-col overflow-scroll bg-slate-100 md:w-[92%] lg:w-5/6 xl:w-[86%]">
         <Navbar />
         {children}
       </div>
