e4048ad8148b429463c7153dca75e44fd199bd39
alkanaziz <alkanaziz@gmail.com>
2024-10-17 15:00:10 +0200

    Add BigCalendar, Announcements, and EventCalendar components to StudentPage

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,7 +1,24 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+import EventCalendar from "@/components/EventCalendar";
+
 const StudentPage = () => {
   return (
-    <div className=''>StudentPage</div>
-  )
-}
+    <div className="flex flex-col gap-4 p-4 xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full rounded-md bg-white p-4">
+          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="flex w-full flex-col gap-8 xl:w-1/3">
+        <EventCalendar />
+        <Announcements />
+      </div>
+    </div>
+  );
+};
 
-export default StudentPage
+export default StudentPage;
