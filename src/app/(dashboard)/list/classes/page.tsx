155b46c83a40651e2a5d644cddf474bbb3c7b34c
alkanaziz <alkanaziz@gmail.com>
2024-10-18 21:19:17 +0200

    Add ClassListPage component to list all classes

---
+++ src/app/(dashboard)/list/classes/page.tsx
@@ -0,0 +1,111 @@
+import Pagination from "@/components/Pagination";
+import Table from "@/components/Table";
+import TableSearch from "@/components/TableSearch";
+import Image from "next/image";
+import { role, classesData } from "@/lib/data";
+import Link from "next/link";
+
+type Class = {
+  id: number;
+  name: string;
+  capacity: number;
+  grade: number;
+  supervisor: string;
+};
+
+const columns = [
+  { header: "Class Name", accessor: "item" },
+  {
+    header: "Capacity",
+    accessor: "capacity",
+  },
+  {
+    header: "Grade",
+    accessor: "grade",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Supervisor",
+    accessor: "supervisor",
+    className: "hidden md:table-cell",
+  },
+  { header: "Actions", accessor: "actions" },
+];
+
+const ClassListPage = () => {
+  const renderRow = (item: Class) => (
+    <tr
+      className="border-b border-gray-300 text-sm even:bg-slate-100 hover:bg-privatPurpleLight"
+      key={item.id}
+    >
+      <td className="flex items-center gap-4 p-4">
+        <h3 className="font-semibold">{item.name}</h3>
+      </td>
+      <td>
+        <span>{item.capacity}</span>
+      </td>
+      <td className="hidden md:table-cell">
+        <span>{item.grade}</span>
+      </td>
+      <td className="hidden md:table-cell">
+        <span>{item.supervisor}</span>
+      </td>
+      <td>
+        <div className="flex items-center gap-2">
+          <Link href={`/list/students/${item.id}`}>
+            <button className="flex size-7 items-center justify-center rounded-full bg-privatSky">
+              <Image src="/view.png" alt="view icon" width={16} height={16} />
+            </button>
+          </Link>
+          {role === "admin" && (
+            <button className="flex size-7 items-center justify-center rounded-full bg-privatPurple">
+              <Image
+                src="/delete.png"
+                alt="delete icon"
+                width={16}
+                height={16}
+              />
+            </button>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+
+  return (
+    <div className="m-4 mt-0 flex-1 rounded-md bg-white p-4">
+      {/* TOP SECTION */}
+      <div className="flex justify-between">
+        <h1 className="hidden text-lg font-semibold md:block">All Students</h1>
+        <div className="flex w-full flex-col items-center justify-center gap-4 md:w-auto md:flex-row">
+          <TableSearch />
+          <div className="flex items-center gap-4 self-end">
+            <button className="rounded-full bg-privatYellow p-2">
+              <Image
+                src="/filter.png"
+                alt="filter icon"
+                width={16}
+                height={16}
+              />
+            </button>
+            <button className="rounded-full bg-privatYellow p-2">
+              <Image src="/sort.png" alt="sort icon" width={16} height={16} />
+            </button>
+            {role === "admin" && (
+              <button className="rounded-full bg-privatYellow p-2">
+                <Image src="/plus.png" alt="plus icon" width={16} height={16} />
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+
+      {/* LIST */}
+      <Table columns={columns} renderRow={renderRow} data={classesData} />
+      {/* PAGINATION */}
+      <Pagination />
+    </div>
+  );
+};
+
+export default ClassListPage;
