72e70be1221547bb2d94188acdb5f6c6843cdaf1
Alkan Aziz <alkanaziz@gmail.com>
2024-10-29 10:27:40 +0100

    Refactor Pagination component to use ITEM_PER_PAGE constant for pagination settings and add functionality to change page on button click

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,22 +1,46 @@
-import Image from "next/image";
+"use client";
 
-const Pagination = () => {
+import { ITEM_PER_PAGE } from "@/lib/settings";
+import { useRouter } from "next/navigation";
+
+const Pagination = ({ page, count }: { page: number; count: number }) => {
+  const router = useRouter();
+  const changePage = (newPage: number) => {
+    const params = new URLSearchParams(window.location.search);
+    params.set("page", newPage.toString());
+    router.push(`${window.location.pathname}?${params}`);
+  };
+
   return (
     <div className="flex items-center justify-between p-4 text-slate-500">
       <button
-        disabled
+        {...(page === 1 && { disabled: true })}
         className="rounded-md bg-slate-200 px-4 py-2 text-xs font-semibold disabled:cursor-not-allowed disabled:opacity-50"
+        onClick={() => changePage(page - 1)}
       >
         Prev
       </button>
       <div className="flex items-center justify-center gap-2 text-sm">
-        <button className="rounded-md bg-privatSky px-2">1</button>
-        <button className="rounded-md px-2">2</button>
-        <button className="rounded-md px-2">3</button>
-        ...
-        <button className="rounded-md px-2">10</button>
+        {Array.from({ length: Math.ceil(count / ITEM_PER_PAGE) }).map(
+          (_, i) => {
+            const pageIndex = i + 1;
+            return (
+              <button
+                key={i + 1}
+                className={`rounded-md px-2 ${page === i + 1 ? "bg-privatSky" : ""}`}
+                onClick={() => changePage(pageIndex)}
+              >
+                {i + 1}
+              </button>
+            );
+          },
+        )}
       </div>
-      <button className="rounded-md bg-slate-200 px-4 py-2 text-xs font-semibold disabled:cursor-not-allowed disabled:opacity-50">
+      <button
+        {...(page === Math.ceil(count / ITEM_PER_PAGE) && { disabled: true })}
+        onClick={() => changePage(page + 1)}
+        className="rounded-md bg-slate-200 px-4 py-2 text-xs font-semibold disabled:cursor-not-allowed disabled:opacity-50"
+      >
         Next
       </button>
     </div>
