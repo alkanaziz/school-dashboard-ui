907302073aa8801ea724572e812452620ef3cc6d
Alkan Aziz <alkanaziz@gmail.com>
2024-10-30 11:40:49 +0100

    Refactor TableSearch component to add search functionality and update form submission

--- src/components/TableSearch.tsx
+++ src/components/TableSearch.tsx
@@ -1,9 +1,27 @@
+"use client";
+
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const TableSearch = () => {
+  const router = useRouter();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const value = (e.currentTarget[0] as HTMLInputElement).value;
+    console.log(value);
+    const params = new URLSearchParams(window.location.search);
+    params.set("search", value.toString());
+    router.push(`${window.location.pathname}?${params}`);
+  };
   return (
     <>
-      <div className="flex w-full items-center justify-between gap-2 rounded-full px-2 text-xs ring-[1.5px] ring-slate-300 md:w-auto">
+      <form
+        onSubmit={handleSubmit}
+        className="flex w-full items-center justify-between gap-2 rounded-full px-2 text-xs ring-[1.5px] ring-slate-300 md:w-auto"
+      >
         <input
           className="w-[200px] bg-transparent p-2 outline-none"
           type="text"
@@ -11,8 +29,10 @@ const TableSearch = () => {
           id=""
           placeholder="Search..."
         />
-        <Image src="/search.png" alt="search icon" width={14} height={14} />
-      </div>
+        <button type="submit">
+          <Image src="/search.png" alt="search icon" width={14} height={14} />
+        </button>
+      </form>
     </>
   );
 };
